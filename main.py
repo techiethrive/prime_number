ebb4a4bab2bd590476c489bf4d13774478bcad9e
Aman Kumar <70050351+theamankumar@users.noreply.github.com>
2021-10-10 14:18:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+n = int(input('Enter any Number: '))
+for i in range(2,n):
+    if n % i == 0:
+        print(f'{n} Not Prime Because it is Divisible by {i} at {n//i}.')
+        break
+else:
+    print(f'{n} is a Prime Number.')
+    
